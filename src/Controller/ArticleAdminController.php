64713263f8d82a44505fd74c78ee2096c238b675
Espinosa Gustavo <age.espinosa@gmail.com>
2020-04-13 13:27:37 -0300

    04- Bind Your Form to a Class

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -23,10 +23,7 @@ class ArticleAdminController extends AbstractController
        $form= $this->createForm(ArticleFormType::class);
        $form->handleRequest($request);
        if($form->isSubmitted() && $form->isValid()){
-           $data= $form->getData();
-           $article= new Article();
-           $article->setTitle($data['title']);
-           $article->setContent($data['content']);
+           $article= $form->getData();
            $article->setAuthor($this->getUser());
 
            $em->persist($article);
